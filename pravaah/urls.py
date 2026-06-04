5655446e8808da42fadfb03c97265676ac15426f
Kavita <kavitashadow27@gmail.com>
2026-06-04 11:44:38 +0530

    My latest user dashboard changes

--- pravaah/urls.py
+++ pravaah/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('usermgmt.urls')),
+    path('users/', include('usermgmt.urls')),
+
 ]
 
 if settings.DEBUG:
